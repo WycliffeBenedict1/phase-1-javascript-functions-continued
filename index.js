590d5c5769bf5ec5dda67ce256e0c1e814173ef1
WycliffeBenedict1 <wycliffebenedict24@gmail.com>
2025-01-11 02:04:26 +0300

    Function lab2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// code your solution here 
+function razzle() {
+    console.log("You've been razzled!");
+  }
+  
+  razzle(); //=> "You've been razzled!"
+function razzle() {
+  console.log("You've been razzled!");
+}
+
+function saturdayFun( activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork( activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
